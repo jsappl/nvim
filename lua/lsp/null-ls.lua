5cd22e0ca6866fe3fe6ac081e6129f5669c92bce
Hessel Tuinhof <pi+git.no.reply@hessel.im>
2021-08-28 00:13:36 +0100

    Replace luaformatter with stylua

--- lua/lsp/null-ls.lua
+++ lua/lsp/null-ls.lua
@@ -3,15 +3,18 @@ local nls = require("null-ls")
 local M = {}
 
 function M.setup(client, buf)
-    nls.config({
-        sources = {
-            nls.builtins.diagnostics.flake8, nls.builtins.diagnostics.luacheck,
-            nls.builtins.formatting.isort,
-            nls.builtins.formatting.prettier.with({
-                filetypes = {"html", "json", "yaml", "markdown", "css"}
-            }), nls.builtins.formatting.yapf, nls.builtins.formatting.lua_format
-        }
-    })
+  nls.config({
+    sources = {
+      nls.builtins.diagnostics.flake8,
+      nls.builtins.diagnostics.luacheck,
+      nls.builtins.formatting.isort,
+      nls.builtins.formatting.prettier.with({
+        filetypes = { "html", "json", "yaml", "markdown", "css" },
+      }),
+      nls.builtins.formatting.yapf,
+      nls.builtins.formatting.stylua,
+    },
+  })
 end
 
 return M
