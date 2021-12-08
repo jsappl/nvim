36bdcc5d5e030bf1c0a4c315721d0f5243eff4d5
Hessel Tuinhof <pi+git.no.reply@hessel.im>
2021-12-08 23:53:42 +0000

    Refactor mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,102 +1,51 @@
--- Mappings for the plugins
+local utils = require("utils")
 
+-- map leader
 vim.g.mapleader = " "
 
--- TODO: Move to separate utils.lua file. Circle dependencies?
-local key_mapper = function(mode, key, result, noremap)
-  noremap = noremap or true
-  vim.api.nvim_set_keymap(mode, key, result, { noremap = noremap, silent = true })
-end
+-- general mappings
 
--- Trouble bindings
--- TODO optimize behavior to own style
-vim.api.nvim_set_keymap("n", "<leader>xx", "<cmd>Trouble<cr>", { silent = true, noremap = true })
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>xw",
-  "<cmd>Trouble lsp_workspace_diagnostics<cr>",
-  { silent = true, noremap = true }
-)
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>xd",
-  "<cmd>Trouble lsp_document_diagnostics<cr>",
-  { silent = true, noremap = true }
-)
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>xl",
-  "<cmd>Trouble loclist<cr>",
-  { silent = true, noremap = true }
-)
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>xq",
-  "<cmd>Trouble quickfix<cr>",
-  { silent = true, noremap = true }
-)
-vim.api.nvim_set_keymap(
-  "n",
-  "gR",
-  "<cmd>Trouble lsp_references<cr>",
-  { silent = true, noremap = true }
-)
+-- plugin mappings
 
--- Tree bindings
-key_mapper("n", "<leader>n", "<cmd>NvimTreeToggle<cr>")
-key_mapper("n", "<leader>m", "<cmd>NvimTreeFindFile<cr>")
+-- trouble
+-- TODO this are example mappings from docs, optimize behavior to own style
+utils.nnoremap("<leader>xx", "<cmd>Trouble<cr>")
+utils.nnoremap("<leader>xw", "<cmd>Trouble lsp_workspace_diagnostics<cr>")
+utils.nnoremap("<leader>xd", "<cmd>Trouble lsp_document_diagnostics<cr>")
+utils.nnoremap("<leader>xl", "<cmd>Trouble loclist<cr>")
+utils.nnoremap("<leader>xq", "<cmd>Trouble quickfix<cr>")
+utils.nnoremap("gR", "<cmd>Trouble lsp_references<cr>")
 
--- fzy bindings
+-- tree explorer
+utils.nnoremap("<leader>n", "<cmd>NvimTreeToggle<cr>")
+utils.nnoremap("<leader>m", "<cmd>NvimTreeFindFile<cr>")
+
+-- fzy
 --TODO move result arguments to plugins.fzy and provide as module. e.g. fzy.find_file, fzy.find_buffer, ...
-vim.api.nvim_set_keymap(
-  "n",
+utils.nnoremap(
   "<leader>ff",
-  "<cmd>lua local fzy = require('plugins.fzy'); fzy.execute('fd', fzy.sinks.edit_file)<cr>",
-  { silent = true, noremap = true }
+  "<cmd>lua local fzy = require('plugins.fzy'); fzy.execute('fd', fzy.sinks.edit_file)<cr>"
 )
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>fb",
-  "<cmd>lua require('plugins.fzy').actions.buffers()<cr>",
-  { silent = true, noremap = true }
-)
-vim.api.nvim_set_keymap(
-  "n",
+utils.nnoremap("<leader>fb", "<cmd>lua require('plugins.fzy').actions.buffers()<cr>")
+utils.nnoremap(
   "<leader>ft",
-  "<cmd>lua local fzy = require('plugins.fzy'); fzy.try(fzy.actions.lsp_tags, fzy.actions.buf_tags)<cr>",
-  { silent = true, noremap = true }
+  "<cmd>lua local fzy = require('plugins.fzy'); fzy.try(fzy.actions.lsp_tags, fzy.actions.buf_tags)<cr>"
 )
-vim.api.nvim_set_keymap(
-  "n",
+utils.nnoremap(
   "<leader>fg",
-  "<cmd>lua local fzy = require('plugins.fzy'); fzy.execute('git ls-files', fzy.sinks.edit_file)<cr>",
-  { silent = true, noremap = true }
+  "<cmd>lua local fzy = require('plugins.fzy'); fzy.execute('git ls-files', fzy.sinks.edit_file)<cr>"
 )
-vim.api.nvim_set_keymap(
-  "n",
+utils.nnoremap(
   "<leader>fl",
-  "<cmd>lua local fzy = require('plugins.fzy'); fzy.execute('ag --nobreak --noheading .', require('utils').fzy_edit_ag_selection)<cr>",
-  { silent = true, noremap = true }
+  "<cmd>lua local fzy = require('plugins.fzy'); fzy.execute('ag --nobreak --noheading .',"
+    .. "require('utils').fzy_edit_ag_selection)<cr>"
 )
 
--- TexLab bindings
-vim.api.nvim_set_keymap("n", "<leader>l", "<cmd>TexlabBuild<cr>", {
-  silent = true,
-  noremap = true,
-})
-vim.api.nvim_set_keymap("n", "<leader>v", "<cmd>TexlabForward<cr>", {
-  silent = true,
-  noremap = true,
-})
+-- texlab
+utils.nnoremap("<leader>l", "<cmd>TexlabBuild<cr>")
+utils.nnoremap("<leader>v", "<cmd>TexlabForward<cr>")
 
 -- Formatting
 -- TODO add range formatting
-vim.api.nvim_set_keymap("n", "<leader>tf", "<cmd>lua require('lsp.formatting').toggle()<CR>", {
-  silent = true,
-  noremap = true,
-})
-
-vim.api.nvim_set_keymap("n", "<leader>b", "<cmd>lua vim.lsp.buf.formatting()<CR>", {
-  silent = true,
-  noremap = true,
-})
+utils.nnoremap("<leader>tf", "<cmd>lua require('lsp.formatting').toggle()<CR>")
+utils.nnoremap("<leader>b", "<cmd>lua vim.lsp.buf.formatting()<CR>")
