2e104dfe7145316b2947cf03345029eda11f9c3c
Hessel Tuinhof <pi+git.no.reply@hessel.im>
2021-12-11 20:42:51 +0000

    tree: Disable window picker

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,6 +1,7 @@
 -- https://github.com/kyazdani42/nvim-tree.lua/issues/674
 vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_git_hl = 1
+vim.g.nvim_tree_disable_window_picker = 1
 
 require("nvim-tree").setup({
   disable_netrw = false,
@@ -9,3 +10,4 @@ require("nvim-tree").setup({
   update_focused_file = { enable = true },
   git = { ignore = true },
 })
+-- Check defaults: https://github.com/kyazdani42/nvim-tree.lua
