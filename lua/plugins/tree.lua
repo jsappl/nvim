99fb02699b5d04cf8176459e46dd4073bf3e27b4
Johannes Sappl <johannes.sappl@chello.at>
2022-03-31 08:51:53 +0200

    refactor(tree): auto_close and window_picker
    Remove auto_close because feature was removed.
    
    Move legacy window_picker option to setup.
    
    Refs: [auto_close](https://github.com/kyazdani42/nvim-tree.lua/commit/524758a207f9c5bf3888b446d9f93192a837b8a7)
    [window_picker](https://github.com/kyazdani42/nvim-tree.lua/commit/69867f4a009675e8a39ae41c516259eb241e9386)

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,13 +1,16 @@
 -- https://github.com/kyazdani42/nvim-tree.lua/issues/674
 vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_git_hl = 1
-vim.g.nvim_tree_disable_window_picker = 1
 
 require("nvim-tree").setup({
   disable_netrw = false,
-  auto_close = true,
   diagnostics = { enable = true },
   update_focused_file = { enable = true },
   git = { ignore = true },
+  open_file = {
+    window_picker = {
+      enable = false,
+    },
+  },
 })
 -- Check defaults: https://github.com/kyazdani42/nvim-tree.lua
