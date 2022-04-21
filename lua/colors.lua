c27ba179ec73eb4b53bf35da594c6f614e03de1d
Johannes Sappl <johannes.sappl@chello.at>
2022-04-21 09:28:41 +0200

    refactor(color): udpate deprecated parser option

--- lua/colors.lua
+++ lua/colors.lua
@@ -11,7 +11,7 @@ require("lualine").setup({
 })
 
 require("nvim-treesitter.configs").setup({
-  ensure_installed = "maintained",
+  ensure_installed = "all",
   highlight = {
     enable = true,
   },
