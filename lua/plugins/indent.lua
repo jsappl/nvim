2197922a5ff667911f875828934fef9be7760c1d
Hessel Tuinhof <pi+git.no.reply@hessel.im>
2023-01-12 22:10:43 +0000

    chore: disable highlight indent context at start
    The color of the underline does not match the vertical context line.
    Also, the corner where vertical and horizonal context lines meet,
    requires manual tweaking of the underline position. Therefore, disables
    the horizonal context underline.

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -5,5 +5,5 @@ vim.opt.listchars:append("eol:↴")
 require("indent_blankline").setup({
   space_char_blankline = " ",
   show_current_context = true,
-  show_current_context_start = true,
+  show_current_context_start = false,
 })
