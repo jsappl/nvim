93dcd750b3751f02e9f02ea7a2f55cda823815f2
Hessel Tuinhof <pi+git.no.reply@hessel.im>
2023-05-02 22:21:06 +0200

    feat: add mypy and fish indent

--- lua/lsp/null-ls.lua
+++ lua/lsp/null-ls.lua
@@ -8,9 +8,11 @@ function M.setup(on_attach)
       nls.builtins.diagnostics.chktex.with({
         args = { "-q", "-f%l:%c:%d:%k:%n:%m\n" , "-I" },
       }),
+      nls.builtins.diagnostics.mypy,
       nls.builtins.diagnostics.flake8,
       nls.builtins.diagnostics.luacheck,
       nls.builtins.diagnostics.shellcheck,
+      nls.builtins.formatting.fish_indent,
       nls.builtins.formatting.isort,
       nls.builtins.formatting.prettier.with({
         filetypes = { "html", "json", "yaml", "markdown", "css" },
