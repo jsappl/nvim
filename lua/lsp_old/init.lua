6359e823ec7b78db56488127f0194f76409a7ab8
Johannes Sappl <johannes.sappl@chello.at>
2024-03-13 23:14:31 +0100

    refactor: rm stale code and files

--- lua/lsp_old/init.lua
+++ lua/lsp_old/init.lua
@@ -1,66 +1,13 @@
--- Init Mason (note: needs to be loaded before lspconfig)
--- TODO
---  * Refactor Mason related setups into custom mason module
---  * Use single ensure_installed list
--- require("mason").setup()
--- require("mason-lspconfig").setup({
---   ensure_installed = {
---     "ltex",
---     "marksman",
---   },
--- })
-
--- require("mason-null-ls").setup({
---   ensure_installed = {
---     -- "black",
---     -- "isort",
---   },
--- })
-
 -- Init null-ls
 local function on_attach(client, bufnr)
   require("lsp.keys").setup(client, bufnr)
-  require("lsp.signature").setup(client, bufnr)
   require("lsp.completion").setup(client, bufnr)
-  require("lsp.formatting").setup(client, bufnr)
 end
 
 require("lsp.null-ls").setup(on_attach)
 
 -- Init language server protocol config
 local nvim_lsp = require("lspconfig")
-
--- local servers = {
---   bashls = {},
---   julials = {},
---   ltex = { language = "en-US" },
---   marksman = {},
---   pyright = {},
---   r_language_server = {},
---   texlab = {
---     settings = {
---       texlab = {
---         auxDirectory = "build",
---         build = {
---           onSave = true,
---           forwardSearchAfter = true,
---           args = {
---             "-pdf",
---             "-shell-escape",
---             "-interaction=nonstopmode",
---             "-synctex=1",
---             "%f",
---             "-outdir=build",
---           },
---         },
---         forwardSearch = {
---           executable = "zathura",
---           args = { "--synctex-forward", "%l:1:%f", "%p" },
---         },
---       },
---     },
---   },
--- }
 
 -- local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
--- lua/plugins_old/indent.lua
+++
@@ -1,7 +0,0 @@
-vim.opt.list = true
-vim.opt.listchars:append("space:·")
-vim.opt.listchars:append("eol:↴")
-
-require("ibl").setup({
-  scope = { show_start = false, show_end = false },
-})
